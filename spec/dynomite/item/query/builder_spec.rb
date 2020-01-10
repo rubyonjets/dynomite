30abe1090efb41d3f2dcabfc55eb7e503e6a34f4
Tung Nguyen <tongueroo@gmail.com>
2020-01-10 18:31:24 +0000

    add build spec with an index

--- spec/dynomite/item/query/builder_spec.rb
+++ spec/dynomite/item/query/builder_spec.rb
@@ -7,13 +7,14 @@ describe Dynomite::Item::Query::Builder do
     builder.instance_variable_set(:@index_finder, index_finder)
     builder
   end
+  let(:index_finder) do
+    finder = double(:null).as_null_object
+    allow(finder).to receive(:find).and_return index
+    finder
+  end
 
-  context "to_params with no indexes" do
-    let(:index_finder) do
-      finder = double(:null).as_null_object
-      allow(finder).to receive(:find).and_return nil
-      finder
-    end
+  context "no indexes" do
+    let(:index) { nil }
 
     it "hash with multiple keys chained" do
       b = builder.where(name: "Tung Nguyen", active: true).where(email: "tung@boltops.com")
@@ -30,12 +31,35 @@ describe Dynomite::Item::Query::Builder do
     it "hash with single key chained" do
       b = builder.where(name: "Tung Nguyen", active: true).where(email: "tung@boltops.com")
       expect(b.to_params).to eq(
-      {:expression_attribute_names=>
-        {"#name"=>"name", "#active"=>"active", "#email"=>"email"},
-       :expression_attribute_values=>
-        {":name"=>"Tung Nguyen", ":active"=>true, ":email"=>"tung@boltops.com"},
-       :filter_expression=>"#name = :name AND #active = :active AND #email = :email",
-       :table_name=>"dynomite_query_builder_testers"}
+        {:expression_attribute_names=>
+          {"#name"=>"name", "#active"=>"active", "#email"=>"email"},
+         :expression_attribute_values=>
+          {":name"=>"Tung Nguyen", ":active"=>true, ":email"=>"tung@boltops.com"},
+         :filter_expression=>"#name = :name AND #active = :active AND #email = :email",
+         :table_name=>"dynomite_query_builder_testers"}
+      )
+    end
+  end
+
+  context "indexes" do
+    let(:index) do
+      index = Dynomite::Item::Indexes::Index.new({})
+      allow(index).to receive(:index_name).and_return("fake-email-index")
+      allow(index).to receive(:fields).and_return(["email"])
+      index
+    end
+
+    it "hash with single key chained" do
+      b = builder.where(name: "Tung Nguyen", active: true).where(email: "tung@boltops.com")
+      expect(b.to_params).to eq(
+        {:expression_attribute_names=>
+          {"#name"=>"name", "#active"=>"active", "#email"=>"email"},
+         :expression_attribute_values=>
+          {":name"=>"Tung Nguyen", ":active"=>true, ":email"=>"tung@boltops.com"},
+         :filter_expression=>"#name = :name AND #active = :active",
+         :key_condition_expression=>"#email = :email",
+         :table_name=>"dynomite_query_builder_testers",
+         :index_name=>"fake-email-index"}
       )
     end
   end
